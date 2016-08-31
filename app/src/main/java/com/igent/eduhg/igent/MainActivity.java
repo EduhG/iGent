3920e5a21aa2382a46decadf43a60bad0cf71803
EduhG <edwinnyangena@gmail.com>
2016-08-31 15:15:19 +0300

    Added items to toolbar

--- app/src/main/java/com/igent/eduhg/igent/MainActivity.java
+++ app/src/main/java/com/igent/eduhg/igent/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -64,5 +68,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int res_id = item.getItemId();
+        if(res_id==R.id.action_settings) {
+            Toast.makeText(getApplicationContext(), "You selected settings", Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 }
